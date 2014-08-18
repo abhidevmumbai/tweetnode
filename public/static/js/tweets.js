75637db32c72cf7baa52aa4e0a8f40fad32fed60
Abhishek Dewadiga <abhishekdewadiga@Abhisheks-MacBook-Pro.local>
2014-08-18 13:33:42 +0530

    Implementing the new nodejs "twit" api for REST search and track Stream calls

--- public/static/js/tweets.js
+++ public/static/js/tweets.js
@@ -1,37 +1,55 @@
+var socket = null;
+
 var client = {
 	tracks_cont: null,
 	tweets_cont: null,
+	// server_url: 'http://192.168.0.207:3000/',
 	server_url: 'http://localhost:3000/',
-	socket: null,
 	users:[],
 	init: function () {
+		console.log('Init...');
 		// Get the DOM elements
 		this.tracks_cont = $('#tracks_cont');
 		this.tweets_cont = $('#tweets_cont');
 
 		if (io !== undefined) {
-			socket = io.connect(this.server_url);
+			socket = io.connect(this.server_url, { 'forceNew': true });
 
-			socket.on('connected', function (r) {
-				console.log('start stream');
-				console.log(r);
-				socket.emit('start stream');
-				// socket.emit('start search');
+			socket.on('connect', function (r) {
+				// socket.emit('start trackStream');
+				socket.emit('start search');
 			});
 
+			// Stream API new tweet
 			socket.on('new tweet', function (tweet) {
 				console.log('New tweet');
 				client.renderTweet(tweet);
 				console.log(tweet);
 			});
+
+			// Search API tweets
+			socket.on('search tweets', function (tweets) {
+				console.log('Search tweet results');
+				client.renderTweetList(tweets);
+			});
 		}		
 	},
 
 	renderTweet: function (tweet) {
+		console.log(tweet);
         var img = '<img src="' + tweet.user.profile_image_url + '" />',
-            link = '<a href="https://twitter.com/' + tweet.user.screen_name + '" target="_blank">' + img + '</a>',
-            elementHtml = '<div class="tweet">' + link + '</div>';
-        this.tweets_cont.append(elementHtml);
+            elementHtml = '<div class="tweet">'
+		        				+ '<a href="https://twitter.com/' + tweet.user.screen_name + '" target="_blank">' + img + '</a>'
+		        				+ '<span>' + tweet.text + '</span>'
+		        			+ '</div>';
+        this.tweets_cont.prepend(elementHtml);
+    },
+
+    renderTweetList: function (tweets) {
+		console.log(tweets);
+		// for (index in tweets.statuses) {
+		// 	setTimeInterval(function () { this.renderTweet(tweets.statuses[index]); }, 500);
+		// }
     }
 }
 
